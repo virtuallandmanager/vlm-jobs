657f9ab1722e3f8016470eed000a180be45d519b
Unknower <dev@unknower.com>
2024-01-02 08:36:38 -0500

    fix connection object type

--- src/services/Redis.service.ts
+++ src/services/Redis.service.ts
@@ -1,4 +1,4 @@
 export const connection = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
+  host: (process.env.REDIS_HOST as string) || "localhost",
+  port: Number(process.env.REDIS_PORT) || 6379,
 };
