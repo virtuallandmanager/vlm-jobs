e29d5dde812897f4e8c80e600899ea3d8aa13eee
Unknower <dev@unknower.com>
2024-01-01 19:19:44 -0500

    remove extra redis config file, fix one that was actually being used

--- src/config/redis.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    port: 6379,
-    host: process.env.REDIS_HOST || "localhost",
-};
--- src/services/Redis.service.ts
+++ src/services/Redis.service.ts
@@ -1,4 +1,4 @@
 export const connection = {
-  host: "localhost",
-  port: 6379,
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT || 6379,
 };
